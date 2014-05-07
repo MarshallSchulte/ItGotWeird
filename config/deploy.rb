1235cb41143c6aaebaf0885fc620905a24cf8bef
MarshallSchulte <marshall.schulte@gmail.com>
2014-05-07 08:45:23 -0400

    Added no turbolinks correctly

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,16 @@ set :deploy_to, '/home/deploy/ItGotWeird'
 
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+end
